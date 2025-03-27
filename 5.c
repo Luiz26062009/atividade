8728d5d2e14a991a59b1c3f14ec0456dd118f556
Luiz26062009 <schultz.luiz@escola.pr.gov.br>
2025-03-27 10:34:13 -0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+    float numero, dolár, resultado;
+     scanf("%f",&numero);
+     dolár=numero*5.73;
+    
+    printf("resultado:%.2f",dolár);
+
+    return 0;
+}
